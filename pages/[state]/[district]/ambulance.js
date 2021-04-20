cd7516e64d80f2dd0e5dd6976daa51419b1ef096
Bodhish Thomas <bodhish@gmail.com>
2021-04-21 04:42:49 +0530

    Add ambulance page

--- pages/[state]/[district]/ambulance.js
+++ pages/[state]/[district]/ambulance.js
@@ -1,41 +1,32 @@
-import Head from 'next/head';
-import { useRouter } from 'next/router';
-import { useState, useEffect } from "react";
-
-import ambulanceData from "../../../temp/ambulance.json";
+import React from "react";
+import { useRouter } from "next/router";
+import { getAmbulances } from "../../../lib/api";
+import { statePaths } from "../../../lib/utils";
 import AmbulanceCard from "../../../components/AmbulanceCard";
 
-export default function Ambulance() {
-  const route = useRouter();
-  let { state, district } = route.query;
-  const [ambulances, setAmbulances] = useState(ambulanceData)
-
-  console.log(state, district, ambulances)
-
-  useEffect(() => {
-    if(state == undefined) state = "";
-    if(district == undefined) district = "";
-    let filteredAmbulances = ambulances.filter((ambulance) => (ambulance.state.toLowerCase().includes(state.toLowerCase()) && ambulance.district.toLowerCase().includes(district.toLowerCase())));
-    setAmbulances(filteredAmbulances);
-  }, [state, district])
-
+export default function Ambulance({ state, district, ambulancesListing }) {
   return (
     <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 p-4">
-      {
-        ambulances.map(({name, phone1, phone2, area, source, id}) => <AmbulanceCard key={id} name={name} phone1={phone1} phone2={phone2} area={area} source={source} />)
-      }
+      {ambulancesListing.map(({ name, phone1, phone2, area, source, id }) => (
+        <AmbulanceCard
+          key={id}
+          name={name}
+          phone1={phone1}
+          phone2={phone2}
+          area={area}
+          source={source}
+        />
+      ))}
     </div>
   );
 }
-
-
 
 export async function getStaticProps({ params }) {
   return {
     props: {
       state: params.state,
       district: params.district,
-      plasmaListing: plasmaByDistrict(params.state, params.district),
+      ambulancesListing: getAmbulances(params.state, params.district),
     },
   };
 }
