b838f0c693be1d13365c5263ff3db3c8d4ad39be
Deepankar Bhade <deep14bhade@gmail.com>
2021-04-21 03:28:50 +0530

    fix: svg attributes

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -22,9 +22,9 @@ const Navbar = () => {
                 aria-hidden='true'
               >
                 <path
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                  stroke-width='2'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
                   d='M4 6h16M4 12h16M4 18h16'
                 />
               </svg>
@@ -37,9 +37,9 @@ const Navbar = () => {
                 aria-hidden='true'
               >
                 <path
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                  stroke-width='2'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
                   d='M6 18L18 6M6 6l12 12'
                 />
               </svg>
@@ -104,9 +104,9 @@ const Navbar = () => {
                 aria-hidden='true'
               >
                 <path
-                  stroke-linecap='round'
-                  stroke-linejoin='round'
-                  stroke-width='2'
+                  strokeLinecap='round'
+                  strokeLinejoin='round'
+                  strokeWidth='2'
                   d='M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9'
                 />
               </svg>
@@ -135,13 +135,13 @@ const Navbar = () => {
                 role='menu'
                 aria-orientation='vertical'
                 aria-labelledby='user-menu-button'
-                tabindex='-1'
+                tabIndex='-1'
               >
                 <a
                   href='#'
                   className='block px-4 py-2 text-sm text-gray-700'
                   role='menuitem'
-                  tabindex='-1'
+                  tabIndex='-1'
                   id='user-menu-item-0'
                 >
                   Your Profile
@@ -150,7 +150,7 @@ const Navbar = () => {
                   href='#'
                   className='block px-4 py-2 text-sm text-gray-700'
                   role='menuitem'
-                  tabindex='-1'
+                  tabIndex='-1'
                   id='user-menu-item-1'
                 >
                   Settings
@@ -159,7 +159,7 @@ const Navbar = () => {
                   href='#'
                   className='block px-4 py-2 text-sm text-gray-700'
                   role='menuitem'
-                  tabindex='-1'
+                  tabIndex='-1'
                   id='user-menu-item-2'
                 >
                   Sign out
