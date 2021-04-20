cc0eb0aec3fe710dfd0eae2ecd705029dc823fd6
developedBySJ <developedbysj@gmail.com>
2021-04-21 02:23:01 +0530

    added search component

--- components/Selector.jsx
+++ components/Selector.jsx
@@ -1,12 +1,64 @@
-import React from 'react';
+import Link from "next/link";
+import React, { useState } from "react";
+import reqData from "../data/states.json";
+
+const state = Object.keys(reqData);
+const district = state.reduce((acc, cur) => {
+  const temp = reqData[cur].map((i) => ({ state: cur, district: i }));
+  return [...acc, ...temp];
+}, []);
 
 const Selector = ({ data }) => {
+  const [searchStr, setSearchStr] = useState("");
+
+  const filterTests = (_data, field = null) => {
+    return _data
+      .filter((i) => {
+        const result = !(
+          String(field ? i[field] : i)
+            .toLowerCase()
+            .search(searchStr.toLowerCase()) === -1
+        );
+        return result;
+      })
+      .splice(0, 5);
+  };
+
   return (
-    <select>
-      {data.map((dt) => {
-        <option value={dt}>{dt}</option>;
-      })}
-    </select>
+    <>
+      <input
+        type="text"
+        className="p-4 w-full focus:border-gray-50"
+        placeholder="Search"
+        value={searchStr}
+        onChange={(e) => setSearchStr(e.target.value)}
+      />
+      {searchStr && (
+        <div className="p-4 bg-gray-300 flex">
+          <div className="w-1/2">
+            <h1>State</h1>
+            {filterTests(state).map((i) => {
+              return (
+                <h1>
+                  <Link href={i.toLowerCase()}>{i}</Link>
+                </h1>
+              );
+            })}
+          </div>
+          <div className="w-1/2">
+            <h1>Dis</h1>
+            {filterTests(district, "district").map((i) => {
+              const url = `/${i.state}/${i.district}`.toLowerCase();
+              return (
+                <h1>
+                  <Link href={url}>{i.district}</Link>
+                </h1>
+              );
+            })}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,71 +1,37 @@
-import Head from 'next/head';
-import Link from 'next/link';
-import { useState } from 'react';
-import { useRouter } from 'next/router';
-import { stateJsonData } from '../data/states';
-
+import Head from "next/head";
+import Link from "next/link";
+import { useState } from "react";
+import { useRouter } from "next/router";
+import { stateJsonData } from "../data/states";
+import Selector from "../components/Selector";
 const tabsArr = [
-  'Oxygen',
-  'Medicine',
-  'Ambulance',
-  'Plasma',
-  'Helpline',
-  'Hospital',
+  "Oxygen",
+  "Medicine",
+  "Ambulance",
+  "Plasma",
+  "Helpline",
+  "Hospital",
 ];
 
 export default function Home() {
-  const [geoState, setGeoState] = useState('');
-  const [geoDistrict, setGeoDistrict] = useState('');
+  const [geoState, setGeoState] = useState("");
+  const [geoDistrict, setGeoDistrict] = useState("");
   const router = useRouter();
   return (
     <div>
       <Head>
         <title>Life | Coronasafe network</title>
-        <link rel='icon' href='/favicon.ico' />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div className='py-4 flex items-center justify-around'>
+      <div className="py-4 flex items-center justify-around">
         {tabsArr.map((tb) => (
           <Link href={`/${tb}`}>
             <p>{tb}</p>
           </Link>
         ))}
       </div>
-      <section></section>
       <section>
-        <select
-          name='state'
-          id='state'
-          value={geoState}
-          onChange={(e) => setGeoState(e.target.value)}
-        >
-          <option value='' disabled>
-            Select State
-          </option>
-          {Object.keys(stateJsonData).map((st) => (
-            <option key={st} value={st}>
-              {st}
-            </option>
-          ))}
-        </select>
-        <select
-          name='district'
-          id='district'
-          value={geoDistrict}
-          onChange={(e) => setGeoDistrict(e.target.value)}
-        >
-          {Object.values(stateJsonData).map((disArr) =>
-            disArr.map((dis) => (
-              <option key={dis} value={dis}>
-                {dis}
-              </option>
-            ))
-          )}
-        </select>
-        <button
-          onClick={() => router.push(`/medicine/${geoState}/${geoDistrict}`)}
-        >
-          Search
-        </button>
+        <Selector />
       </section>
     </div>
   );
