e8cf6745b55f33b241dfde59a2156fee0b53130e
Abhinandan Arya <abhinandanarya06122000@gmail.com>
2021-04-21 14:12:06 +0530

    replace class with className

--- components/OxygenCard.jsx
+++ components/OxygenCard.jsx
@@ -19,22 +19,22 @@ const OxygenCard = ({ name, company, phone1, phone2, description, source, slink,
         </div>
 
         <div>
-          <div class="">
-            {phone1 ? <div class="-ml-px w-0 flex-1 flex">
-              <a href={`tel:${phone1}`} class="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500">
+          <div className="">
+            {phone1 ? <div className="-ml-px w-0 flex-1 flex">
+              <a href={`tel:${phone1}`} className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500">
                 <svg className="w-5 h-5 text-gray-400" x-description="Heroicon name: solid/phone" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                   <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
                 </svg>
-                <span class="ml-3">{phone1}</span>
+                <span className="ml-3">{phone1}</span>
               </a>
             </div> : < div/>}
 
-            {phone2 ? <div class="-ml-px w-0 flex-1 flex">
-              <a href={`tel:${phone2}`} class="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500">
-                <svg class="w-5 h-5 text-gray-400" x-description="Heroicon name: solid/phone" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+            {phone2 ? <div className="-ml-px w-0 flex-1 flex">
+              <a href={`tel:${phone2}`} className="relative w-0 flex-1 inline-flex items-center justify-center py-4 text-sm text-gray-700 font-medium border border-transparent rounded-br-lg hover:text-gray-500">
+                <svg className="w-5 h-5 text-gray-400" x-description="Heroicon name: solid/phone" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                   <path d="M2 3a1 1 0 011-1h2.153a1 1 0 01.986.836l.74 4.435a1 1 0 01-.54 1.06l-1.548.773a11.037 11.037 0 006.105 6.105l.774-1.548a1 1 0 011.059-.54l4.435.74a1 1 0 01.836.986V17a1 1 0 01-1 1h-2C7.82 18 2 12.18 2 5V3z"></path>
                 </svg>
-                <span class="ml-3">{phone2}</span>
+                <span className="ml-3">{phone2}</span>
               </a>
             </div> : < div/>}
           </div>
