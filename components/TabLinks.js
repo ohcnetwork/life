03b83badabe724156f167f94a3ea09932b75923a
Deepankar Bhade <deep14bhade@gmail.com>
2021-04-22 19:04:47 +0530

    chore: remove unused imports

--- components/TabLinks.js
+++ components/TabLinks.js
@@ -1,8 +1,8 @@
-import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { useRouter } from "next/router";
-import Link from "next/link";
-import { parametreize, humanize } from "../lib/utils";
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useRouter } from 'next/router';
+import Link from 'next/link';
+import { parametreize } from '../lib/utils';
 
 export default function TabLinks({ state, district, tabsInfo }) {
   const router = useRouter();
@@ -12,27 +12,27 @@ export default function TabLinks({ state, district, tabsInfo }) {
   // const activeStyle = defaultStyle + "border-indigo-500 text-indigo-600";
 
   return (
-    <div className="border-b border-gray-200">
-      <nav className="flex flex-wrap" aria-label="Tabs">
-        <div className="flex flex-wrap overflow-hidden space-x-1">
+    <div className='border-b border-gray-200'>
+      <nav className='flex flex-wrap' aria-label='Tabs'>
+        <div className='flex flex-wrap overflow-hidden space-x-1'>
           {tabsInfo.map((tab) => {
             const render = (
               <div
                 className={`w-5/12 overflow-hidden ${
                   district[tab.value]
-                    ? "bg-" + tab.color.split("-")[1] + "-200 cursor-pointer"
-                    : " cursor-not-allowed"
+                    ? 'bg-' + tab.color.split('-')[1] + '-200 cursor-pointer'
+                    : ' cursor-not-allowed'
                 }`}
               >
                 <div
                   className={`w-min border-transparent flex items-center justify-center px-4 py-2 text-center ${
                     district[tab.value]
-                      ? "text-" + tab.color.split("-")[1] + "-900"
-                      : "text-gray-500"
+                      ? 'text-' + tab.color.split('-')[1] + '-900'
+                      : 'text-gray-500'
                   } group border-b-2 font-medium`}
                 >
-                  <FontAwesomeIcon icon={tab.icon} className="w-5" />
-                  <span className="ml-2">{tab.name}</span>
+                  <FontAwesomeIcon icon={tab.icon} className='w-5' />
+                  <span className='ml-2'>{tab.name}</span>
                 </div>
               </div>
             );
