0a89b1df94d12c0794ca554a415253067b43c8fe
Deepankar Bhade <deep14bhade@gmail.com>
2021-04-23 09:22:14 +0530

    add purgecss to tailwindconfig

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './pages/**/*.{js,jsx,ts,tsx}',
+    './layouts/**/*.{js,jsx,ts,tsx}',
+    './components/**/*.{js,jsx,ts,tsx}',
+    './public/index.html',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
