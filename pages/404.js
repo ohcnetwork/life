c40fb5c701addfbc5fbda291ed97300dff494b78
Abhinandan Arya <abhinandanarya06122000@gmail.com>
2021-04-23 16:27:02 +0530

    404 page improved

--- pages/404.js
+++ pages/404.js
@@ -4,14 +4,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 const TextCompnent = () => {
   return (
     <div className="p-5 mx-auto">
-      <div className="font-bold text-4xl">Oops! No data found</div>
-      <div className="p-10 pl-0 text-xl">
+      <div className="font-bold text-2xl md:text-4xl">Oops! No data found</div>
+      <div className="p-10 pl-0 text-sm md:text-xl">
         The page you're looking for is now beyond our reach. Let's get you
       </div>
       <div>
         <a
           href="/"
-          className="underline font-bold text-gray-600 hover:text-black hover:shadow-lg p-3"
+          className="text-sm lg:text-base underline font-bold text-gray-600 hover:text-black hover:shadow-lg p-3"
         >
           HOME PAGE
         </a>
@@ -22,21 +22,23 @@ const TextCompnent = () => {
 
 const Component404 = () => {
   return (
-    <div className="error_404_lettersBox">
-      <div className="error_404_letters flex">
-        <div className="error_404_letter4 error_404_letter">
-          <h1 className="txt404">4</h1>
+    <div>
+      <div className="flex items-center text-center justify-center">
+        <div className="font-mono text-5xl md:text-9xl font-semibold flex items-center">
+          <h1 className="flex items-center">4</h1>
         </div>
-        <div className="error_404_letter0 error_404_letter ml-16 mr-2 mt-6">
-          <h1 className="txt404">
+        <div className="font-mono font-semibold flex items-center ml-16 mr-2 mt-6 h-auto">
+          <h1 className="flex items-center">
             <FontAwesomeIcon
               icon={faVirus}
-              className="virusImg mx-auto text-9xl"
+              size="xs"
+              className="virusImg text-red-300"
+              spin
             />
           </h1>
         </div>
-        <div className="error_404_letter4 error_404_letter">
-          <h1 className="txt404">4</h1>
+        <div className="font-mono text-5xl md:text-9xl font-semibold flex items-center">
+          <h1 className="font-mono text-5xl md:text-9xl font-semibold flex items-center">4</h1>
         </div>
       </div>
     </div>
@@ -45,11 +47,12 @@ const Component404 = () => {
 
 export default function Custom404() {
   return (
-    <div>
-      <div className="pt-56">
+    <div className="mt-10 md:mt-40 w-11/12 md:w-3/4 mx-auto">
+      <div className="pt-2">
         <div className="sm:hidden">
           <div className="items-center">
             <Component404 />
+            <br/>
             <TextCompnent />
           </div>
         </div>
