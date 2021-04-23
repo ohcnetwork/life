ef656221bddd2fa27d8333a13a791436df8ba6c2
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-04-23 17:07:21 +0530

    🌙 Add Dark Mode to 404 Page

--- pages/404.js
+++ pages/404.js
@@ -1,20 +1,21 @@
-import { faVirus } from "@fortawesome/free-solid-svg-icons";
+import { faArrowRight, faVirus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link"
 
 const TextCompnent = () => {
   return (
     <div className="p-5 mx-auto">
-      <div className="font-bold text-2xl md:text-4xl">Oops! No data found</div>
-      <div className="p-10 pl-0 text-sm md:text-xl">
+      <div className="font-bold text-2xl md:text-4xl dark:text-gray-200">Oops! No data found</div>
+      <div className="p-10 pl-0 text-sm md:text-xl dark:text-gray-400">
         The page you're looking for is now beyond our reach. Let's get you
       </div>
       <div>
-        <a
-          href="/"
-          className="text-sm lg:text-base underline font-bold text-gray-600 hover:text-black hover:shadow-lg p-3"
-        >
-          HOME PAGE
-        </a>
+        <Link href="/" >
+          <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+            Home Page
+            <FontAwesomeIcon icon={faArrowRight} className="w-3 mx-2" />
+          </button>
+        </Link>
       </div>
     </div>
   );
@@ -25,20 +26,20 @@ const Component404 = () => {
     <div>
       <div className="flex items-center text-center justify-center">
         <div className="font-mono text-5xl md:text-9xl font-semibold flex items-center">
-          <h1 className="flex items-center">4</h1>
+          <h1 className="flex items-center dark:text-gray-100">4</h1>
         </div>
         <div className="font-mono font-semibold flex items-center ml-16 mr-2 mt-6 h-auto">
           <h1 className="flex items-center">
             <FontAwesomeIcon
               icon={faVirus}
               size="xs"
-              className="virusImg text-red-300"
+              className="virusImg text-red-300 dark:text-primary-400"
               spin
             />
           </h1>
         </div>
         <div className="font-mono text-5xl md:text-9xl font-semibold flex items-center">
-          <h1 className="font-mono text-5xl md:text-9xl font-semibold flex items-center">4</h1>
+          <h1 className="font-mono text-5xl md:text-9xl font-semibold flex items-center dark:text-gray-100">4</h1>
         </div>
       </div>
     </div>
@@ -47,12 +48,12 @@ const Component404 = () => {
 
 export default function Custom404() {
   return (
-    <div className="pt-10 md:pt-40 w-11/12 md:w-3/4 mx-auto">
-      <div className="pt-2">
+    <div className="py-16 md:py-20 w-11/12 md:w-3/4 mx-auto">
+      <div className="py-8">
         <div className="sm:hidden">
           <div className="items-center">
             <Component404 />
-            <br/>
+            <br />
             <TextCompnent />
           </div>
         </div>
