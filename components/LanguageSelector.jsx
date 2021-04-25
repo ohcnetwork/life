c4e31e95e64a53ebb6dfa4416b6ce265b352c7d7
ProCode2 <bpro249@gmail.com>
2021-04-25 22:17:49 +0530

    feat: add translate button
    Co-authored-by: Deepankar Bhade <statisticsaffinity@gmail.com>

--- components/LanguageSelector.jsx
+++ components/LanguageSelector.jsx
@@ -5,7 +5,7 @@ const LanguageSelector = () => {
     const { locale, setLocale } = useLocaleContext();
     return (
         <select
-            className="cursor-pointer fixed top-3 left-3 font-semibold py-2 px-4  text-md"
+            className="cursor-pointer fixed top-3 left-3 font-semibold text-sm rounded shadow h-8 dark:bg-gray-900 dark:text-white"
             value={locale}
             onChange={(e) => setLocale(e.target.value)}>
             <option value="EN">English</option>
