7f4f265f6fb25c52b89fa33820afb0eeec6fb2b2
karmakoder <vinil.bhandari@gmail.com>
2021-04-25 18:19:53 -0400

    Adding stats on home page. https://github.com/coronasafe/life/issues/40

--- pages/index.jsx
+++ pages/index.jsx
@@ -9,6 +9,8 @@ import Selector from '@components/Selector';
 import { tabsInfo } from '@lib/tabs';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChartBar, faMedkit } from '@fortawesome/free-solid-svg-icons';
+import hospitalCareCenterData from './../data/hospital_clinic_centre.json';
+import ambulanceData from './../data/ambulance.json';
 
 let updateFilter = (setSelectedFilter, selection) => setSelectedFilter(selection);
 
@@ -57,6 +59,7 @@ export default function Home() {
                             Covid19 Statistics
                         </button>
                     </a>
+
                     {/* <a href="/oxygen_requirements">
                         <button
                             type="button"
@@ -69,6 +72,21 @@ export default function Home() {
                             Oxygen Requirements
                         </button>
                     </a> */}
+                </div>
+                <div className="flex space-x-3">
+                    <div
+                        type="button"
+                        className="inline-flex items-center px-3 py-2 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-md dark:text-white text-black dark:bg-gray-1000 bg-white hover:opacity-60 focus:outline-none mt-6">
+                    <FontAwesomeIcon
+                        className="text-white-400 w-4 mr-4"
+                        title="Covid 19 Statistics"
+                        icon={faChartBar}
+                    />Total Hospitals : {Object.keys(hospitalCareCenterData.data).length}(Verified :{hospitalCareCenterData.data.filter(value => value.verificationStatus === 'Verified').length}) |
+                     Total Ambulance : {Object.keys(ambulanceData.data).length}
+                                         (Verified :
+                                          {ambulanceData.data.filter(value => value.verificationStatus === 'Verified').length})
+
+                                                         </div>
                 </div>
             </section>
         </div>
