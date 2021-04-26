67a77e52b5b86f4c2c76ce4333b8b970967f0b74
ProCode2 <bpro249@gmail.com>
2021-04-26 23:33:27 +0530

    logically displaying data

--- components/AboutQA.jsx
+++ components/AboutQA.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlusCircle } from '@fortawesome/free-solid-svg-icons';
+import { faMinusCircle, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 
 function AboutQA({ question, answer }) {
     const [open, setOpen] = useState(false);
@@ -8,9 +8,21 @@ function AboutQA({ question, answer }) {
         <div className="w-full border-2 border-blue-500 shadow rounded-md py-4 px-8">
             <div className="flex justify-between items-center w-full">
                 <div className="">{question}</div>
-                <FontAwesomeIcon className="w-10 cursor-pointer" icon={faPlusCircle} />
+                {!open ? (
+                    <FontAwesomeIcon
+                        onClick={() => setOpen(!open)}
+                        className="w-10 cursor-pointer"
+                        icon={faPlusCircle}
+                    />
+                ) : (
+                    <FontAwesomeIcon
+                        onClick={() => setOpen(!open)}
+                        className="w-10 cursor-pointer"
+                        icon={faMinusCircle}
+                    />
+                )}
             </div>
-            {open && <div>{answer}</div>}
+            {open && <div className="text-sm text-gray-800 px-8 mt-3">{answer}</div>}
         </div>
     );
 }
