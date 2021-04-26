4d5bee5ad27a2ce47fb26c0db1acd1f18cea84d5
ProCode2 <bpro249@gmail.com>
2021-04-27 00:16:14 +0530

    give icons a specific width

--- components/AboutQA.jsx
+++ components/AboutQA.jsx
@@ -5,22 +5,24 @@ import { faMinusCircle, faPlusCircle } from '@fortawesome/free-solid-svg-icons';
 function AboutQA({ question, answer }) {
     const [open, setOpen] = useState(false);
     return (
-        <div className="w-full border-2 border-blue-500 shadow-md rounded-md py-4 px-8">
+        <div className="w-full border-2 border-blue-500 shadow-md rounded-md py-4 md:px-8 px-2">
             <div className="flex justify-between items-center w-full">
-                <div className="w-4/5">{question}</div>
-                {!open ? (
-                    <FontAwesomeIcon
-                        onClick={() => setOpen(!open)}
-                        className="w-1/5 cursor-pointer"
-                        icon={faPlusCircle}
-                    />
-                ) : (
-                    <FontAwesomeIcon
-                        onClick={() => setOpen(!open)}
-                        className="w-10 cursor-pointer"
-                        icon={faMinusCircle}
-                    />
-                )}
+                <div className="w-3/4 mr-1">{question}</div>
+                <div className="w-1/4">
+                    {!open ? (
+                        <FontAwesomeIcon
+                            onClick={() => setOpen(!open)}
+                            className="w-10 ml-auto cursor-pointer"
+                            icon={faPlusCircle}
+                        />
+                    ) : (
+                        <FontAwesomeIcon
+                            onClick={() => setOpen(!open)}
+                            className="w-10 ml-auto cursor-pointer"
+                            icon={faMinusCircle}
+                        />
+                    )}
+                </div>
             </div>
             {open && <div className="text-sm text-gray-800 mt-3">{answer.map((ans) => ans)}</div>}
         </div>
