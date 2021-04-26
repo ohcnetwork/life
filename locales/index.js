9b970c3070f7a2f4f50eb33645a71a18af1f31ab
Deepankar Bhade <deep14bhade@gmail.com>
2021-04-26 07:22:47 +0530

    chore: creating list of lang available

---
+++ locales/index.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        name: 'English',
+        code: 'EN'
+    },
+    {
+        name: 'Hindi',
+        code: 'HI'
+    },
+    {
+        name: 'Marathi',
+        code: 'MR'
+    }
+];
