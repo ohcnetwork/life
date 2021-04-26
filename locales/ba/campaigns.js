913fe4b0d733c339036e68739d593a83ea981cc5
ProCode2 <bpro249@gmail.com>
2021-04-26 21:07:11 +0530

    adding ba translation to /campaigns page

--- locales/ba/campaigns.js
+++ locales/ba/campaigns.js
@@ -1,11 +1,21 @@
 export default {
-    text: `As of today, India is undergoing a severe second wave with the world’s highest daily cases. Hospitals and healthcare providers are running out of resources in the fight against COVID-19 and the situation on the ground is dire. Oxygen is critically required to treat patients affected with COVID, many of whom require hospitalization due to fluctuating oxygen levels. However, there is a severe shortage of oxygen availability across hospitals in India. Oxygen concentrators are alternate devices to oxygen cylinders – while cylinders contain a finite amount of oxygen supply, a concentrator recycles oxygen from the air and delivers it to the patient. They can be used to manage patient requirements without looking for external sources of oxygen.
+    text: `আজ অবধি, ভারত কোভিড ১৯ এর তীব্র দ্বিতীয় তরঙ্গের মুখোমুখি হয়েছে। COVID-19 এর বিরুদ্ধে লড়াইয়ে হাসপাতাল এবং স্বাস্থ্যসেবা সরবরাহকারীদের পর্যাপ্ত সুবিধা নেই এবং পরিস্থিতি ভয়াবহ। অক্সিজেন বিশেষত কোভিড ১৯ দ্বারা আক্রান্ত রোগীদের চিকিত্সার জন্য প্রয়োজনীয়, যাদের অনেকের অক্সিজেনের মাত্রা ওঠানামা করার কারণে হাসপাতালে ভর্তি প্রয়োজন। তবে ভারতে সমস্ত হাসপাতাল জুড়ে অক্সিজেনের সহজলভ্যতার মারাত্মক ঘাটতি রয়েছে। অক্সিজেন কনসেন্ট্রেটরগুলি অক্সিজেন সিলিন্ডারের বিকল্প ডিভাইস হয় - যখন সিলিন্ডারে সীমাবদ্ধ পরিমাণে অক্সিজেন সরবরাহ থাকে, এক কনসেন্টারেটর বাতাস থেকে অক্সিজেন পুনরুদ্ধার করে এবং এটি রোগীর কাছে সরবরাহ করে। অক্সিজেনের বাহ্যিক উত্সগুলি অনুসন্ধান না করে এগুলি রোগীর প্রয়োজনীয়তা পরিচালনা করতে ব্যবহার করা যেতে পারে।
 
-  Swasth Digital Health Foundation (Swasth.app) along with ACT Grants is looking to procure oxygen concentrators and channel them to hospitals across levels of care in remote areas for COVID response and long-term health-system strengthening. These oxygen concentrators can save many thousands of lives and bolster much-needed resources for hospitals. They can also be used to treat outpatients, reducing the load on hospital beds and easing the stress faced by patients.
-  • A high flow concentrator costs Rs.85000 and treat up to 550 patients with a severe or critical illness.
+  Swasth Digital Health Foundation (Swasth.app) along with ACT Grants এরা অক্সিজেন কনসেন্টেটর সংগ্রহ করতে এবং সিওভিড প্রতিক্রিয়া এবং দীর্ঘমেয়াদী স্বাস্থ্য-ব্যবস্থার শক্তিশালীকরণের জন্য প্রত্যন্ত অঞ্চলে যত্নের স্তরের বিভিন্ন হাসপাতালে তাদের চ্যানেল সরবরাহ করছে। এই অক্সিজেন কনসেন্ট্রেটরগুলি হাজার হাজার জীবন বাঁচাতে এবং হাসপাতালের জন্য প্রয়োজনীয় প্রয়োজনীয় সংস্থানগুলি বাড়িয়ে তুলতে পারে। এগুলি বহিরাগত রোগীদের চিকিত্সা করতে, হাসপাতালের বিছানার ভার বোঝা কমাতে এবং রোগীদের দ্বারা আসা চাপ কমাতেও ব্যবহার করা যেতে পারে।
+  • একটি উচ্চ ফ্লো কনসেন্ট্রেটারের দাম 85000 টাকা এবং গুরুতর বা গুরুতর অসুস্থতায় 550 জন রোগীর চিকিত্সা করা হয়।
   • A low flow concentrator costs Rs.45000 and can treat up to 900 patients with mild to moderate illness
 
   Swasth is raising funds to support this effort. Please contribute by donating at: https://www.impactguru.com/fundraiser/oxygen
   The distribution and allocation of concentrators will be a transparent process with the method and impact metrics published openly so you can be assured of your contributions saving lives.
   If you are a company looking to deploy a CSR budget or an individual donor willing to contribute, please email us at shubha@swasthapp.com. We look forward to your support.`
 };
+
+// Oxygen concentrators are alternate devices to oxygen cylinders – while cylinders contain a finite amount of oxygen supply, a concentrator recycles oxygen from the air and delivers it to the patient. They can be used to manage patient requirements without looking for external sources of oxygen.
+
+//   Swasth Digital Health Foundation (Swasth.app) along with ACT Grants is looking to procure oxygen concentrators and channel them to hospitals across levels of care in remote areas for COVID response and long-term health-system strengthening. These oxygen concentrators can save many thousands of lives and bolster much-needed resources for hospitals. They can also be used to treat outpatients, reducing the load on hospital beds and easing the stress faced by patients.
+//   • A high flow concentrator costs Rs.85000 and treat up to 550 patients with a severe or critical illness.
+//   • A low flow concentrator costs Rs.45000 and can treat up to 900 patients with mild to moderate illness
+
+//   Swasth is raising funds to support this effort. Please contribute by donating at: https://www.impactguru.com/fundraiser/oxygen
+//   The distribution and allocation of concentrators will be a transparent process with the method and impact metrics published openly so you can be assured of your contributions saving lives.
+//   If you are a company looking to deploy a CSR budget or an individual donor willing to contribute, please email us at shubha@swasthapp.com. We look forward to your support.
