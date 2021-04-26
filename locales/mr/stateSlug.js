12750bf56da60d7cb8e1e556e753b45865a8060e
Deepankar Bhade <deep14bhade@gmail.com>
2021-04-27 00:33:39 +0530

    trans: marathi stateSlug.js

--- locales/mr/stateSlug.js
+++ locales/mr/stateSlug.js
@@ -1,5 +1,5 @@
 export default {
     share: `शेयर`,
-    searchResFor: `Search Result For`,
-    searchPlaceholder: `Search Districts For`
+    searchResFor: `शोध माहिती करा`,
+    searchPlaceholder: `जिल्हा शोधा `
 };
