db2c1126bee53bfaedcd6238a07fcb3ee74b5bab
Deepankar Bhade <deep14bhade@gmail.com>
2021-04-27 21:01:58 +0530

    feat: videos [id] card

--- pages/videos/[id].js
+++ pages/videos/[id].js
@@ -1,3 +1,5 @@
+import Breadcumb from '@components/Breadcumb';
+import VideoCard from '@components/VideoCard';
 import { videoData } from '@data/video';
 
 export const getStaticProps = async ({ params }) => {
@@ -21,7 +23,19 @@ export const getStaticPaths = async () => {
 };
 
 const VideoSingle = ({ video }) => {
-    return <div>{video['Title']}</div>;
+    return (
+        <div>
+            <section className="pt-10">
+                <div>
+                    <Breadcumb list={[{ href: null, name: `${video['Title']}` }]} />
+                </div>
+                <h2 class="text-3xl mb-5 md:text-4xl xl:text-5xl tracking-tight mt-4 font-bold leading-tight dark:text-white">
+                    Videos
+                </h2>
+                <VideoCard dt={video} />
+            </section>
+        </div>
+    );
 };
 
 export default VideoSingle;
