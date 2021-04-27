22d312b795b739f1c5bcaf47961acff737d6067a
vandanabhandari <vandana.premkumar@gmail.com>
2021-04-27 19:27:34 -0400

    Updating review comments

--- scraper/src/v2_json_downloader.py
+++ scraper/src/v2_json_downloader.py
@@ -15,16 +15,21 @@ def generate_v2_file(url, data, name=""):
     f.close()
 
 
-def parseOxygen(data):
+def parseInfo(info_type, data):
     active_district_data_v2_data = {
         "data": []
     }
     json_array = json.loads(data)
-    for oxygen_data in json_array:
+    for info_type_json in json_array:
         active_district_json = dict(ambulance="false", contact="false", district="", doctor="false", helpline="false",
                                     hospitals="false", medicine="false", oxygen="false", state="")
-        active_district_json["state"] = oxygen_data["state_name"]
-        active_district_json["district"] = oxygen_data["district_name"]
+        active_district_json["state"] = info_type_json["state_name"]
+        active_district_json["district"] = info_type_json["district_name"]
+        if(info_type == "oxygen"): #Update the logic for oxygen to be true based on values in oxygen.json
+            active_district_json["oxygen"] = "true"
+        #if(info_type == "ambulance" && TODO_LOGIC_FOR_AMBULENCE_TRUE):  #Update the logic for ambulance to be true based on values in ambulance.json
+            #active_district_json["ambulance"] = "true"
+
         active_district_data_v2_data["data"].append(active_district_json)
     generate_v2_file("active_district_data", active_district_data_v2_data)
 
@@ -36,10 +41,13 @@ headers = {'User-Agent': user_agent, }
 
 list_of_life_files = {
     "oxygen": {
-        "url": "https://life_data.coronasafe.network/oxygen.json",
-        "parseFunction": parseOxygen
+        "url": "https://life_data.coronasafe.network/oxygen.json"
     }
-}  # There will be more keys for vaccine, ambulance ...
+    # Idea is there will be more keys for vaccine, ambulance ...
+    #,"ambulance": {
+    #    "url": "https://life_data.coronasafe.network/ambulance.json"
+    #}
+}
 
 for life_file_key in list_of_life_files:
     life_file = list_of_life_files[life_file_key]["url"]
@@ -47,4 +55,5 @@ for life_file_key in list_of_life_files:
     response = urllib.request.urlopen(request)
     data = response.read()
     generate_v2_file(url=life_file, data={"data" : json.loads(data.decode())})
-    list_of_life_files[life_file_key]["parseFunction"](data)
+    parseInfo(data.decode().split("/")[-1].split(".")[0], data)
+    #list_of_life_files[life_file_key]["parseFunction"](data)
