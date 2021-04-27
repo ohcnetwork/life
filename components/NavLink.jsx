f92ddc1ceebe7e975c1fbba8f42661780333e7e8
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-04-27 22:16:26 +0530

    📱 Make NavBar more Responsive

--- components/NavLink.jsx
+++ components/NavLink.jsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const NavLink = ({ title, link, icon }) => {
     const router = useRouter();
-    const defaultStyle = "flex items-center text-gray-900 dark:text-gray-300 px-2 py-2 md:p-0 mx-5 md:mx-6 md:pb-1 ";
+    const defaultStyle = "flex items-center text-right text-gray-900 dark:text-gray-300 px-2 py-2 md:p-0 mx-5 md:mx-6 md:pb-1 ";
     const activeStyle = defaultStyle + "font-semibold text-indigo-800 dark:text-white border-b-2 border-primary-600"
 
     link = link.split("#")[0]
@@ -12,8 +12,8 @@ const NavLink = ({ title, link, icon }) => {
     return (
         <a href={link}
             className={(router.pathname.startsWith(link) ? activeStyle : defaultStyle)}>
-            <FontAwesomeIcon icon={icon} className="w-3 mr-2" />
-            {title}
+            <FontAwesomeIcon icon={icon} className="w-3 mr-2 ml-auto" />
+            <span>{title}</span>
         </a>
     );
 }
