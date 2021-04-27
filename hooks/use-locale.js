7e8551d55bd11ce4dd91e53ff21c9ab5ae0d709e
Deepankar Bhade <deep14bhade@gmail.com>
2021-04-27 08:09:49 +0530

    feat: new func to handle edge cases

--- hooks/use-locale.js
+++ hooks/use-locale.js
@@ -9,20 +9,33 @@ import kn from '@locales/kn/index';
 import ta from '@locales/ta/index';
 
 const languages = {
-    'EN': en,
-    'MR': mr,
-    'HI': hi,
-    'TE': te,
-    'BA': ba,
-    'PA': pa,
-    'GU': gu,
-    'KN': kn,
-    'TA': ta
-}
+    EN: en,
+    MR: mr,
+    HI: hi,
+    TE: te,
+    BA: ba,
+    PA: pa,
+    GU: gu,
+    KN: kn,
+    TA: ta
+};
 
-const useLocale = (loc) => {
-    const locale = languages[loc] || languages["EN"]
-    return locale;
+const transParse = (lang, type) => {
+    // if the type doesn't exist on the lang send the default
+    if (lang[type] === undefined) {
+        return en[type];
+    } else {
+        for (let x in en[type]) {
+            if (lang[type][x] === undefined) {
+                lang[type][x] = en[type][x];
+            }
+        }
+        return lang[type];
+    }
+};
+
+const useLocale = (loc, type) => {
+    return transParse(languages[loc], type);
 };
 
 export default useLocale;
