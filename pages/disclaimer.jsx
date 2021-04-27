c3a7f045b6329f0ef6978d7610882284714e308c
Deepankar Bhade <deep14bhade@gmail.com>
2021-04-27 14:41:37 +0530

    fix: build errors

--- pages/disclaimer.jsx
+++ pages/disclaimer.jsx
@@ -4,24 +4,24 @@ import { useLocaleContext } from '@hooks/use-locale-context';
 import React from 'react';
 const Disclaimer = () => {
     const { locale } = useLocaleContext();
-    const t = useLocale(locale).about;
+    const t = useLocale(locale, 'about');
 
     return (
-        <section className="">
+        <section className="dark:text-gray-100 text-gray-1000">
             <div className="pt-10">
                 <Breadcumb list={[{ href: null, name: t.q11 }]} />
             </div>
-            <h1 className="text-3xl font-bold">{t.q11}</h1>
+            <h1 className="text-3xl mt-10 font-bold">{t.q11}</h1>
             <section className="py-3 flex flex-col space-y-2">
                 <p>
                     {t.a112}
                     {
                         <a
-                            className="text-indigo-600 font-semibold dark:text-gray-600 mt-2 text-md"
+                            className="text-indigo-500 font-semibold mt-2 text-md"
                             href="https://liferesources.in/"
                             target="_blank">
                             https://liferesources.in/
-                                    </a>
+                        </a>
                     }
                     {t.a113}
                 </p>
@@ -29,6 +29,6 @@ const Disclaimer = () => {
             </section>
         </section>
     );
-}
+};
 
 export default Disclaimer;
