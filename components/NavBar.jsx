52c21901ffde20bacb914e709ad4357becdd1c3c
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-04-27 23:02:29 +0530

    📱 Add Hamburger Menu to Navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocaleContext } from '@hooks/use-locale-context';
 import Logo from './Logo';
 import NavLink from './NavLink';
@@ -10,24 +10,18 @@ import {
     faHandsHelping,
     faHeart,
     faBookOpen,
+    faBars,
 } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const NavBar = () => {
 
     const { locale } = useLocaleContext()
     const t = useLocale(locale, 'home');
 
+    const [isOpen, setIsOpen] = useState(false);
+
     const navLinks = [
-        {
-            icon: faDatabase,
-            title: `${t.contributeData}`,
-            link: '/data'
-        },
-        {
-            icon: faHandsHelping,
-            title: `${t.partnerWithUs}`,
-            link: '/about#partner'
-        },
         {
             icon: faBookOpen,
             title: `${t.learn}`,
@@ -38,14 +32,27 @@ const NavBar = () => {
             title: `${t.campaigns}`,
             link: '/campaigns'
         },
+        {
+            icon: faHandsHelping,
+            title: `${t.partnerWithUs}`,
+            link: '/about#partner'
+        },
+        {
+            icon: faDatabase,
+            title: `${t.contributeData}`,
+            link: '/data'
+        },
 
     ]
 
     return (
         <section className="mb-20">
-            <nav className="flex bg-gray-200 dark:bg-gray-1000 items-center justify-between px-5 fixed top-0 left-0 w-full z-50 md:shadow-sm">
+            <nav className="flex bg-gray-200 dark:bg-gray-1200 items-center justify-between px-5 py-2 fixed top-0 left-0 w-full z-50 md:shadow-sm">
+                <div onClick={() => setIsOpen(prev => !prev)} className="flex items-center m-3 cursor-pointer md:hidden">
+                    <FontAwesomeIcon icon={faBars} className="w-3" />
+                </div>
                 <a href="/" className="flex items-center m-3">
-                    <Logo width={65} />
+                    <Logo height={40} />
                     <h1 className="ml-1 uppercase font-black text-3xl text-gray-900 dark:text-gray-100">
                         {t.title}
                     </h1>
@@ -66,13 +73,16 @@ const NavBar = () => {
                     </div>
                 </div>
             </nav>
-            <div className='flex bg-gray-200 dark:bg-gray-1000 py-2 flex-wrap justify-between md:hidden px-4 mt-20'>
-                {
-                    navLinks.map(({ icon, title, link }, id) =>
-                        <NavLink key={id} title={title} link={link} icon={icon} />
-                    )
-                }
-            </div>
+            {
+                isOpen &&
+                <div className='flex bg-gray-200 dark:bg-gray-1000 py-2 flex-col items-center justify-center md:hidden px-4 mt-20'>
+                    {
+                        navLinks.map(({ icon, title, link }, id) =>
+                            <NavLink key={id} title={title} link={link} icon={icon} />
+                        )
+                    }
+                </div>
+            }
         </section>
     );
 }
--- components/NavLink.jsx
+++ components/NavLink.jsx
@@ -4,14 +4,12 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const NavLink = ({ title, link, icon }) => {
     const router = useRouter();
-    const defaultStyle = "flex items-center text-right text-gray-900 dark:text-gray-300 px-2 py-2 md:p-0 mx-5 md:mx-6 md:pb-1 ";
+    const defaultStyle = "flex items-center text-right text-gray-900 dark:text-gray-300 px-2 py-2 md:p-0 mx-5 md:mx-6 md:pb-1 hover:text-gray-700 ";
     const activeStyle = defaultStyle + "font-semibold text-indigo-800 dark:text-white border-b-2 border-primary-600"
-
-    link = link.split("#")[0]
 
     return (
         <a href={link}
-            className={(router.pathname.startsWith(link) ? activeStyle : defaultStyle)}>
+            className={(router.pathname.startsWith(link.split("#")[0]) ? activeStyle : defaultStyle)}>
             <FontAwesomeIcon icon={icon} className="w-3 mr-2 ml-auto" />
             <span>{title}</span>
         </a>
