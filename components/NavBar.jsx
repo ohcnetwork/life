bff6c977d060e8b8381e701b607a0d568803815e
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-04-27 21:27:31 +0530

    😼 Add FA Icons to Menu Items

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -5,8 +5,12 @@ import NavLink from './NavLink';
 import LanguageSelector from './LanguageSelector';
 import ThemeButton from './ThemeButton';
 import useLocale from '@hooks/use-locale';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+    faDatabase,
+    faHandsHelping,
+    faHeart,
+    faBookOpen,
+} from '@fortawesome/free-solid-svg-icons';
 
 const NavBar = () => {
 
@@ -14,15 +18,32 @@ const NavBar = () => {
     const t = useLocale(locale, 'home');
 
     const navLinks = [
-        { title: "COVID-19 Statistics", link: "https://www.covid19india.org/" },
-        { title: "Contribute Verified Data", link: "/data" },
-        { title: "Partner with Us", link: "/about#partner" },
-        { title: "Campaigns", link: "/campaigns" },
+        {
+            icon: faDatabase,
+            title: `${t.contributeData}`,
+            link: '/data'
+        },
+        {
+            icon: faHandsHelping,
+            title: `${t.partnerWithUs}`,
+            link: '/about#partner'
+        },
+        {
+            icon: faBookOpen,
+            title: `${t.learn}`,
+            link: '/learn'
+        },
+        {
+            icon: faHeart,
+            title: `${t.campaigns}`,
+            link: '/campaigns'
+        },
+
     ]
 
     return (
         <section className="mb-20">
-            <nav className="flex bg-gray-200 dark:bg-gray-1000 items-center justify-between px-5 fixed top-0 left-0 w-full">
+            <nav className="flex bg-gray-200 dark:bg-gray-1000 items-center justify-between px-5 fixed top-0 left-0 w-full z-50 md:shadow-sm">
                 <a href="/" className="flex items-center m-3">
                     <Logo width={65} />
                     <h1 className="ml-1 uppercase font-black text-3xl text-gray-900 dark:text-gray-100">
@@ -32,23 +53,23 @@ const NavBar = () => {
                 <div className="flex-1 flex items-center justify-end">
                     <div className="items-center justify-end hidden md:flex">
                         {
-                            navLinks.map((navItem, id) =>
-                                <NavLink key={id} title={navItem.title} link={navItem.link} />
+                            navLinks.map(({ icon, title, link }, id) =>
+                                <NavLink key={id} title={title} link={link} icon={icon} />
                             )
                         }
                     </div>
-                    <div className="flex items-center mx-4">
+                    <div className="flex items-center mx-4 mb-1">
                         <LanguageSelector />
                     </div>
-                    <div className="flex items-center mx-4">
+                    <div className="flex items-center mx-4 mb-1">
                         <ThemeButton />
                     </div>
                 </div>
             </nav>
-            <div className='flex bg-gray-200 dark:bg-gray-1000 py-3 flex-wrap justify-between md:hidden px-4 mt-20'>
+            <div className='flex bg-gray-200 dark:bg-gray-1000 py-2 flex-wrap justify-between md:hidden px-4 mt-20'>
                 {
-                    navLinks.map((navItem, id) =>
-                        <NavLink key={id} title={navItem.title} link={navItem.link} />
+                    navLinks.map(({ icon, title, link }, id) =>
+                        <NavLink key={id} title={title} link={link} icon={icon} />
                     )
                 }
             </div>
--- components/NavLink.jsx
+++ components/NavLink.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { useRouter } from "next/router";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const NavLink = ({ title, link }) => {
+const NavLink = ({ title, link, icon }) => {
     const router = useRouter();
-    const defaultStyle = "text-gray-1100 dark:text-gray-300 p-2 md:p-0 mx-0 md:mx-6 md:pb-2 ";
+    const defaultStyle = "flex items-center text-gray-900 dark:text-gray-300 px-2 py-2 md:p-0 mx-5 md:mx-6 md:pb-1 ";
     const activeStyle = defaultStyle + "font-semibold text-indigo-800 dark:text-white border-b-2 border-primary-600"
 
     link = link.split("#")[0]
@@ -11,6 +12,7 @@ const NavLink = ({ title, link }) => {
     return (
         <a href={link}
             className={(router.pathname.startsWith(link) ? activeStyle : defaultStyle)}>
+            <FontAwesomeIcon icon={icon} className="w-3 mr-2" />
             {title}
         </a>
     );
