fd3f0160a9fcfd2b8ac9489d52a3a92b6499a412
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-04-27 20:40:35 +0530

    🎨 Redesign Main Layout
    Co-authored-by: Sandra Kakkarayil Jayakumar <sandrakakkarayil01@gmail.com>

--- layouts/MainLayout.jsx
+++ layouts/MainLayout.jsx
@@ -1,12 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import Footer from '@components/Footer';
-import ThemeButton from '@components/ThemeButton';
 import { LocaleContext } from '@hooks/use-locale-context';
-import LanguageSelector from '@components/LanguageSelector';
+import NavBar from '@components/NavBar';
 
 const MainLayout = ({ children }) => {
     const [locale, setBaseLocal] = useState('EN');
     const value = { locale, setBaseLocal };
+   
     useEffect(() => {
         if (typeof window !== 'undefined') {
             if (
@@ -24,11 +24,8 @@ const MainLayout = ({ children }) => {
     return (
         <LocaleContext.Provider value={value}>
             <div className="flex flex-col items-stretch min-h-screen bg-gray-100 dark:bg-gray-1100">
-                <div className="relative flex-shrink-0">
-                    <ThemeButton />
-                    <LanguageSelector />
-                </div>
-                <div className="max-w-5xl container mx-auto px-2 pb-6 flex-grow flex-shrink-0">
+                <NavBar />                
+                <div className="max-w-5xl mx-auto px-2 pb-6 flex-grow flex-shrink-0">
                     {children}
                 </div>
                 <Footer />
