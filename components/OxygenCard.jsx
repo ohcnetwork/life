f7ad532a060ce9718648c2245e8869bc60662e1a
Deepankar Bhade <deep14bhade@gmail.com>
2021-04-28 11:22:37 +0530

    chore: cleaning up

--- components/OxygenCard.jsx
+++ components/OxygenCard.jsx
@@ -11,66 +11,76 @@ import SocialSharing from '@components/SocialSharing';
 
 function OxygenCard({ data }) {
     const checkValue = (value) => {
-        if(value) return value;
+        if (value) return value;
         return (
-            <span>
-                {value || (
-                    <FontAwesomeIcon icon={faQuestionCircle} className="w-3 ml-2" />
-                )}
-            </span>
+            <span>{value || <FontAwesomeIcon icon={faQuestionCircle} className="w-3 ml-2" />}</span>
         );
     };
     return (
         <tr>
-            <td class="px-6 py-4">
-                <div class="flex items-center">
-                    <div class="ml-4">
-                        <div class="text-sm font-medium text-gray-900">
-                        { data.name }
-                        </div>
-                        <div class="text-sm text-gray-500">
-                        { data.type }
-                        </div>
+            <td className="px-6 py-4">
+                <div className="flex items-center">
+                    <div className="ml-4">
+                        <div className="text-sm font-medium text-gray-900">{data.name}</div>
+                        <div className="text-sm text-gray-500">{data.type}</div>
                     </div>
                 </div>
             </td>
-            <td class="px-6 py-4">
-                <div class="text-sm text-gray-900">{ data.villageTownCity + " " + data.district + " " + data.state + " - " + data.pincode }</div>
-                <div class="text-sm text-gray-500">{ data.secondaryDistrictsAndStates.split(" ").join(" | ") }</div>
+            <td className="px-6 py-4">
+                <div className="text-sm text-gray-900">
+                    {data.villageTownCity +
+                        ' ' +
+                        data.district +
+                        ' ' +
+                        data.state +
+                        ' - ' +
+                        data.pincode}
+                </div>
+                <div className="text-sm text-gray-500">
+                    {data.secondaryDistrictsAndStates.split(' ').join(' | ')}
+                </div>
             </td>
-            <td class="px-6 py-4 font-bold">
+            <td className="px-6 py-4 font-bold">
                 <div className="flex">
                     <div className="text-sm text-gray-500 mr-1">HIGH:</div>
-                    <div class="text-sm text-gray-900">{ checkValue(data.highFlowOxygenConcentratorsRequired) }</div>
+                    <div className="text-sm text-gray-900">
+                        {checkValue(data.highFlowOxygenConcentratorsRequired)}
+                    </div>
                 </div>
                 <div className="flex">
                     <div className="text-sm text-gray-500 mr-1">LOW:</div>
-                    <div class="text-sm text-gray-900">{ checkValue(data.lowFlowOxygenConcentratorsRequired) }</div>
+                    <div className="text-sm text-gray-900">
+                        {checkValue(data.lowFlowOxygenConcentratorsRequired)}
+                    </div>
                 </div>
             </td>
-            <td class="px-6 py-4 font-bold">
+            <td className="px-6 py-4 font-bold">
                 <div className="flex">
                     <div className="text-sm text-gray-500 mr-1">COVID:</div>
-                    <div class="text-sm text-gray-900">{ checkValue(data.covidBeds) }</div>
+                    <div className="text-sm text-gray-900">{checkValue(data.covidBeds)}</div>
                 </div>
                 <div className="flex">
                     <div className="text-sm text-gray-500 mr-1">TOTAL:</div>
-                    <div class="text-sm text-gray-900">{ checkValue(data.beds) }</div>
+                    <div className="text-sm text-gray-900">{checkValue(data.beds)}</div>
                 </div>
             </td>
-            <td class="px-6 py-4 font-bold">
+            <td className="px-6 py-4 font-bold">
                 <div className="flex">
                     <div className="text-sm text-gray-500 mr-1">SERVED:</div>
-                    <div class="text-sm text-gray-900">{ checkValue(data.approximatePatientsServedPerDay) }</div>
+                    <div className="text-sm text-gray-900">
+                        {checkValue(data.approximatePatientsServedPerDay)}
+                    </div>
                 </div>
                 <div className="flex">
                     <div className="text-sm text-gray-500 mr-1">TOTAL:</div>
-                    <div class="text-sm text-gray-900">{ checkValue(data.crateringToPeople) }</div>
+                    <div className="text-sm text-gray-900">
+                        {checkValue(data.crateringToPeople)}
+                    </div>
                 </div>
             </td>
-            <td class="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
-                <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-gray-600 text-black">
-                    { data.govtOrNonGovt }
+            <td className="px-6 py-4 whitespace-nowrap text-center text-sm font-medium">
+                <span className="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-gray-600 text-black">
+                    {data.govtOrNonGovt}
                 </span>
             </td>
         </tr>
@@ -78,31 +88,3 @@ function OxygenCard({ data }) {
 }
 
 export default OxygenCard;
-
-
-{/* <div className="w-full bg-white rounded-lg shadow dark:bg-gray-1200 dark:text-gray-300 pb-3 hover:shadow-lg">
-    <div className="w-full flex justify-between items-center pt-2">
-                <div className="ml-4 font-mono text-xs font-bold text-white dark:text-black rounded-full py-1 px-2 bg-gray-600">
-                    {' '}
-                    {data.govtOrNonGovt || 'Govt'}
-                </div>
-                <div className="ml-auto">
-                    <SocialSharing
-                        url={'https://life.coronasafe.network/oxygen_requirements'}
-                        twitterText={` More Info: https://life.coronasafe.network/oxygen_requirements`}
-                        copyText={`
-                                    Name: ${data.name}
-                                    State: ${data.state}
-                                    District: ${data.district}
-                                    Patients served perday: ~${data.approximatePatientsServedPerDay}
-                                    Hign flow oxygen required: ${data.highFlowOxygenConcentratorsRequired}
-                                    Low flow oxygen required: ${data.lowFlowOxygenConcentratorsRequired}
-                                    PPE kits required: ${data.ppeKitsRequired}
-                                    Remesivir required: ${data.remdesivirRequired}
-                                    Tocilizumab required: ${data.tocilizumabRequired}
-                                    Favipiravir required: ${data.favipiravirRequired}
-                                    Other items required: ${data.otherItemsRequired}
-                                    `}
-                    />
-                </div>
-            </div>         </div>*/}
--- pages/oxygen_requirements.jsx
+++ pages/oxygen_requirements.jsx
@@ -23,42 +23,54 @@ function OxygenRequirements({ oxygenReqData }) {
     return (
         <div>
             <NextSeo {...SEO} />
-            <section className="flex flex-col ml-2 md:pt-10">
+            <section className="flex flex-col ml-2 pt-10">
                 <Breadcumb list={[{ href: null, name: 'Oxygen Requirement' }]} />
             </section>
 
-            <section class="flex flex-col">
-                <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-                    <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                    <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
-                        <table class="min-w-full divide-y divide-gray-200">
-                        <thead class="bg-gray-50">
-                            <tr>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Name
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Location
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Oxygen Concentrators Required
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Beds
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Capacity
-                                </th>
-                                <th scope="col" class="relative px-6 py-3">
-                                    <span class="sr-only" />
-                                </th>
-                            </tr>
-                        </thead>
-                        <tbody class="bg-white divide-y divide-gray-200">
-                            { oxygenReqData.map((data) => <OxygenCard key={data.name + data.district} data={data} />) }
-                        </tbody>
-                        </table>
-                    </div>
+            <section className="flex flex-col pt-5">
+                <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                        <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                            <table className="min-w-full divide-y divide-gray-200">
+                                <thead className="bg-gray-50">
+                                    <tr>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Name
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Location
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Oxygen Concentrators Required
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Beds
+                                        </th>
+                                        <th
+                                            scope="col"
+                                            className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                            Capacity
+                                        </th>
+                                        <th scope="col" className="relative px-6 py-3">
+                                            <span className="sr-only" />
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody className="bg-white divide-y divide-gray-200">
+                                    {oxygenReqData.map((data) => (
+                                        <OxygenCard key={data.name + data.district} data={data} />
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </section>
