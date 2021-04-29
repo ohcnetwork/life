1a744647a847bb6fe4a0716fb40fd268e53c86af
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-04-29 09:43:18 +0530

    🚀 Clean Up Helper Functions

--- pages/index.jsx
+++ pages/index.jsx
@@ -41,16 +41,12 @@ export default function Home({ state, district, type }) {
 
     const [districtChoosen, setDistrictChoosen] = useState(district || districts[0]);
 
-    const mapDisToCity = (s) => {
-        // checks if it exists
-        // maps "North Delhi" -> "Delhi"
-        const temp = districtMapCity[s];
+    const generatePageURL = (_state, _district, _resource) =>
+        `/${parametreize(_state)}/${parametreize(_district)}/${parametreize(_resource)}`
 
-        if (temp === undefined) {
-            return s;
-        } else {
-            return temp;
-        }
+    const mapDistrictToCity = (s) => {
+        // checks if it exists maps "North Delhi" -> "Delhi"
+        return districtMapCity[s] || s;
     };
 
     const [resourceChoosen, setResourceChoosen] = useState(type || 'Oxygen');
@@ -76,31 +72,31 @@ export default function Home({ state, district, type }) {
         Vaccine: getVaccine(parametreize(stateChoosen), parametreize(districtChoosen), true)
     };
 
-    const handleChooseState = (s) => {
-        setStateChoosen(s);
-        const newDistrict = statesWithDistricts[s][0];
+    const handleChooseState = ({ target: { value } }) => {
+        setStateChoosen(value);
+        const newDistrict = statesWithDistricts[value][0];
         setDistrictChoosen(newDistrict);
         router.push(
-            `/${parametreize(s)}/${parametreize(newDistrict)}/${parametreize(resourceChoosen)}`
+            generatePageURL(value, newDistrict, resourceChoosen)
         );
     };
 
-    const handleDistrictChange = (d) => {
-        setDistrictChoosen(d);
+    const handleDistrictChange = ({ target: { value } }) => {
+        setDistrictChoosen(value);
         router.push(
-            `/${parametreize(stateChoosen)}/${parametreize(d)}/${parametreize(resourceChoosen)}`
+            generatePageURL(stateChoosen, value, resourceChoosen)
         );
     };
 
-    const handleResourceChange = (v) => {
+    const handleResourceChange = ({ target: { value } }) => {
+        setResourceChoosen(value);
         router.push(
-            `/${parametreize(stateChoosen)}/${parametreize(districtChoosen)}/${parametreize(v)}`
+            generatePageURL(stateChoosen, districtChoosen, value)
         );
-        setResourceChoosen(v);
     };
 
     return (
-        <section className="max-w-7xl mx-auto px-2">
+        <section className="w-full mx-auto px-2">
             <div className="bg-gray-200 dark:bg-gray-1200 text-center pt-5 pb-20">
                 <h1 className="font-semibold text-xl dark:text-gray-300">{t.description}</h1>
             </div>
@@ -108,13 +104,13 @@ export default function Home({ state, district, type }) {
                 <section className="bg-white dark:bg-gray-1300 rounded-lg mx-12 p-5 shadow-lg flex flex-col md:flex-row md:items-center">
                     <div className="flex flex-col flex-1 my-2 md:my-0 dark:text-gray-200">
                         <label htmlFor="state" className="text-sm">
-                            {t.select} {t.state}{' '}
+                            {t.select} {t.state}
                         </label>
                         <div className="flex items-center relative">
                             <select
                                 id="state"
                                 value={stateChoosen}
-                                onChange={({ target: { value } }) => handleChooseState(value)}
+                                onChange={handleChooseState}
                                 className="py-2 w-full font-bold text-xl outline-none bg-transparent dark:text-gray-400 rounded-md my-2 appearance-none pr-3 cursor-pointer z-10">
                                 {states.map((s, id) => (
                                     <option
@@ -125,7 +121,7 @@ export default function Home({ state, district, type }) {
                                     </option>
                                 ))}
                             </select>
-                            <div className="absolute right-1.5">
+                            <div className="absolute right-0">
                                 <FontAwesomeIcon icon={faAngleDown} className="w-5" />
                             </div>
                         </div>
@@ -139,7 +135,7 @@ export default function Home({ state, district, type }) {
                             <select
                                 id="district"
                                 value={districtChoosen}
-                                onChange={({ target: { value } }) => handleDistrictChange(value)}
+                                onChange={handleDistrictChange}
                                 className="py-2  w-full font-bold text-xl outline-none bg-transparent dark:text-gray-400 rounded-md my-2 appearance-none pr-3 cursor-pointer z-10">
                                 {districts.map((s, id) => (
                                     <option className="dark:text-gray-900" key={id} value={s}>
@@ -161,7 +157,7 @@ export default function Home({ state, district, type }) {
                             <select
                                 id="resource"
                                 value={resourceChoosen}
-                                onChange={({ target: { value } }) => handleResourceChange(value)}
+                                onChange={handleResourceChange}
                                 className="py-2 w-full font-bold text-xl outline-none bg-transparent dark:text-gray-400 rounded-md my-2 appearance-none pr-3 cursor-pointer z-10">
                                 {Object.keys(resources).map((s, id) => (
                                     <option className="dark:text-gray-900" key={id} value={s}>
@@ -176,9 +172,7 @@ export default function Home({ state, district, type }) {
                     </div>
                     <div className="flex flex-col ml-0 md:ml-6 w-full md:w-min">
                         <Link
-                            href={`/${parametreize(stateChoosen)}/${parametreize(
-                                districtChoosen
-                            )}/${parametreize(resourceChoosen)}`}>
+                            href={generatePageURL(stateChoosen, districtChoosen, resourceChoosen)}>
                             <button className="bg-indigo-600 hover:bg-indigo-700 w-full md:w-14 h-16 rounded-md cursor-pointer text-gray-200 flex justify-center items-center">
                                 <span>
                                     <FontAwesomeIcon icon={faSearch} className="w-5" />
@@ -192,11 +186,10 @@ export default function Home({ state, district, type }) {
                     <>
                         <div className="dark:text-gray-200 mx-auto max-w-xs text-base flex font-bold justify-around my-5 cursor-pointer">
                             <div
-                                className={`w-3/6 flex justify-center items-center pb-2  ${
-                                    tabVal === 'result'
-                                        ? `border-b-2 border-gray-900 dark:border-gray-300`
-                                        : ``
-                                }`}
+                                className={`w-3/6 flex justify-center items-center pb-2  ${tabVal === 'result'
+                                    ? `border-b-2 border-gray-900 dark:border-gray-300`
+                                    : ``
+                                    }`}
                                 onClick={() => changeTabs('result')}>
                                 <FontAwesomeIcon
                                     className="text-gray-900 dark:text-gray-300 mr-2"
@@ -206,11 +199,10 @@ export default function Home({ state, district, type }) {
                                 Results
                             </div>
                             <div
-                                className={`w-3/6 flex justify-center items-center pb-2  ${
-                                    tabVal === 'twitter'
-                                        ? `border-b-2 border-gray-900 dark:border-gray-300`
-                                        : ``
-                                }`}
+                                className={`w-3/6 flex justify-center items-center pb-2  ${tabVal === 'twitter'
+                                    ? `border-b-2 border-gray-900 dark:border-gray-300`
+                                    : ``
+                                    }`}
                                 onClick={() => changeTabs('twitter')}>
                                 <FontAwesomeIcon
                                     className="text-blue-500 mr-2"
@@ -224,7 +216,7 @@ export default function Home({ state, district, type }) {
                             <SearchResult type={type} resources={resources[type]} />
                         )}
                         {tabVal === 'twitter' && (
-                            <TwitterContainer searchStr={mapDisToCity(districtChoosen)} />
+                            <TwitterContainer searchStr={mapDistrictToCity(districtChoosen)} />
                         )}
                     </>
                 ) : (
