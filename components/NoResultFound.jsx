eb2674957362992d7d38819982686da2e7c9ae14
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-04-29 12:52:53 +0530

    🎨 Tweek Colors in Tabs

--- components/NoResultFound.jsx
+++ components/NoResultFound.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NoResultFound = ({ type, district, state }) => {
     return (
         <div className="flex flex-col items-center justify-center mt-6 dark:text-gray-300">
-            <img src="/icons/noResults.svg" className="w-1/5" alt="No Data Found!" />
+            <img src="/icons/noResults.svg" className="w-1/4 md:w-1/6 my-2" alt="No Data Found!" />
             <span>No Data Found for</span>
             <span className="font-semibold">{type} in {district}, {state}</span>
         </div>
