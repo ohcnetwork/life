ad3cf55b1992c64ef65371c59e71a5a7994d8514
Deepankar Bhade <deep14bhade@gmail.com>
2021-04-29 10:45:23 +0530

    feat: refactor twitter result

--- components/TwitterResult.jsx
+++ components/TwitterResult.jsx
@@ -10,24 +10,35 @@ function TwitterResultCard({ covidConnectResults, searchStr }) {
                             Live Twitter Results for "{searchStr}"
                         </div>
                         <div className="py-3 text-sm">
-                            {covidConnectResults.map((result) => (
-                                <a
-                                    href={`https://twitter.com/i/web/status/${result.id}`}
-                                    target="_blank"
-                                    rel="noopener noreferrer"
-                                    key={result.id}>
-                                    <div className="flex justify-start cursor-pointer text-gray-700  hover:bg-gray-200 dark:hover:bg-gray-1100 rounded-md px-2 py-2 my-2">
-                                        <div className="px-2">
-                                            <div className="text-sm dark:text-primary-400 font-normal text-black-500 tracking-wide">
-                                                {parseDateString(result.created_at)}
+                            {covidConnectResults.length > 0 ? (
+                                <>
+                                    <p className="ml-3">
+                                        Showing {covidConnectResults.length} Results
+                                    </p>
+                                    {covidConnectResults.map((result) => (
+                                        <a
+                                            href={`https://twitter.com/i/web/status/${result.id}`}
+                                            target="_blank"
+                                            rel="noopener noreferrer"
+                                            key={result.id}>
+                                            <div className="flex justify-start cursor-pointer text-gray-700  hover:bg-gray-200 dark:hover:bg-gray-1100 rounded-md px-2 py-2 my-2">
+                                                <div className="px-2">
+                                                    <div className="text-sm dark:text-primary-400 font-normal text-black-500 tracking-wide">
+                                                        {parseDateString(result.created_at)}
+                                                    </div>
+                                                    <div className="flex-grow text-black dark:text-white font-semibold">
+                                                        {result.text}
+                                                    </div>
+                                                </div>
                                             </div>
-                                            <div className="flex-grow text-black dark:text-white font-semibold">
-                                                {result.text}
-                                            </div>
-                                        </div>
-                                    </div>
-                                </a>
-                            ))}
+                                        </a>
+                                    ))}
+                                </>
+                            ) : (
+                                <div className="pl-3 text-lg dark:text-gray-500">
+                                    <p>No Results Found for {searchStr}</p>
+                                </div>
+                            )}
                         </div>
                     </div>
                 </div>
