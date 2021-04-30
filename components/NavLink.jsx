0f81b7f7d862397c4133c329aee1ac51882c1672
Arnab Sen <arnabsen1729@gmail.com>
2021-04-30 20:40:16 +0530

    Fix: replaced Fa Icons in the navbar with custom components

--- components/NavLink.jsx
+++ components/NavLink.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
-import { useRouter } from "next/router";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useRouter } from 'next/router';
 
 const NavLink = ({ title, link, icon }) => {
     const router = useRouter();
-    const defaultStyle = "flex items-center text-right text-gray-900 dark:text-gray-300 px-2 py-2 md:p-0 mx-5 md:mx-6 md:pb-1 hover:text-gray-700 ";
-    const activeStyle = defaultStyle + "font-semibold text-indigo-800 dark:text-white border-b-2 border-primary-600"
+    const defaultStyle =
+        'flex items-center text-right text-gray-900 dark:text-gray-300 px-2 py-2 md:p-0 mx-5 md:mx-6 md:pb-1 hover:text-gray-700 ';
+    const activeStyle =
+        defaultStyle +
+        'font-semibold text-indigo-800 dark:text-white border-b-2 border-primary-600';
 
+    const defaultStyleIcon = 'fill-current text-gray-900 dark:text-gray-300 mr-2 ml-auto ';
+    const activeStyleIcon = defaultStyleIcon + 'text-indigo-800 dark:text-white';
     return (
-        <a href={link}
-            className={(router.pathname.startsWith(link.split("#")[0]) ? activeStyle : defaultStyle)}>
-            <FontAwesomeIcon icon={icon} className="w-3 mr-2 ml-auto" />
+        <a
+            href={link}
+            className={router.pathname.startsWith(link.split('#')[0]) ? activeStyle : defaultStyle}>
+            <span
+                className={
+                    router.pathname.startsWith(link.split('#')[0])
+                        ? activeStyleIcon
+                        : defaultStyleIcon
+                }>
+                {icon}
+            </span>
             <span>{title}</span>
         </a>
     );
-}
+};
 
 export default NavLink;
