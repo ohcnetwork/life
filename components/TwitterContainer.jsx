26ab672728d6b1917e02f65ee1d83484e5047aeb
khavinshankar <khavinshankar@gmail.com>
2021-05-01 23:59:51 +0530

    cleanup, removed unused imports

--- components/TwitterContainer.jsx
+++ components/TwitterContainer.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import TwitterResultCard from '@components/TwitterResult';
 import { useFetch } from '@components/Selector'
 
--- pages/[state]/[district]/index.js
+++ pages/[state]/[district]/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { statePaths, parametreize, humanize, activeDistricts } from '@lib/utils';
+import { statePaths, humanize } from '@lib/utils';
 import { NextSeo } from 'next-seo';
 import DetailedHome from '@components/DetailedHome';
 
