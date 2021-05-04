c93545af7fe9da27071aa8d8761c1c5853ed9c6f
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-05-05 00:00:40 +0530

    🎨 Add Trending Icon

---
+++ components/icons/TrendingIcon.js
@@ -0,0 +1,19 @@
+import React from 'react';
+const TrendingIcon = (props) => {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            {...props}
+            viewBox="0 0 20 20"
+            fill="currentColor"
+        >
+            <path
+                fillRule="evenodd"
+                d="M12 7a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0V8.414l-4.293 4.293a1 1 0 01-1.414 0L8 10.414l-4.293 4.293a1 1 0 01-1.414-1.414l5-5a1 1 0 011.414 0L11 10.586 14.586 7H12z"
+                clipRule="evenodd"
+            />
+        </svg>
+    );
+}
+
+export default TrendingIcon;
