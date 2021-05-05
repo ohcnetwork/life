ab75bbaf6f972509a7ed0186e1175ea51ce5fc67
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-05-05 12:03:21 +0530

    📌 Label Unused Component

--- components/StartSearching.jsx
+++ components/StartSearching.jsx
@@ -6,7 +6,7 @@ const StartSearching = ({ text, res }) => {
         <div className="flex flex-col items-center justify-center dark:text-gray-300 mt-6">
             <img src="/icons/startSearching.svg" className="w-1/5" alt="Start Searching" />
             <span className="font-semibold">
-                {text} {totalResources()}+ {res}
+                {text} {totalResources().total}+ {res}
             </span>
         </div>
     );
