4ee13b7581c1af6fb93c51eb75dd5d7a578c4444
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-05-05 13:39:22 +0530

    🚜 Move Top States and Districts to Data

---
+++ data/top_states_and_districts.js
@@ -0,0 +1,16 @@
+export default [
+    { name: "Maharashtra", type: "State" },
+    { name: "Kerala", type: "State" },
+    { name: "Karnataka", type: "State" },
+    { name: "Uttar Pradesh", type: "State" },
+    { name: "Tamil Nadu", type: "State" },
+    { name: "Delhi (NCT)", type: "State" },
+    { name: "Pune", type: "District", state: "Maharashtra" },
+    { name: "Bengaluru (Bangalore) Urban", type: "District", state: "Karnataka" },
+    { name: "Mumbai City", type: "District", state: "Maharashtra" },
+    { name: "Thane", type: "District", state: "Maharashtra" },
+    { name: "Nagpur", type: "District", state: "Maharashtra" },
+    { name: "Chennai", type: "District", state: "Tamil Nadu" },
+    { name: "Nashik", type: "District", state: "Maharashtra" },
+    { name: "Ernakulam", type: "District", state: "Kerala" }
+];
