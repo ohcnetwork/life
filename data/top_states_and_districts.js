29f4ac85478472971c15a71d6bfd20a402a98e8a
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-05-06 16:34:49 +0530

    📐 Make Top States and Districts Dynamic

--- data/top_states_and_districts.js
+++ data/top_states_and_districts.js
@@ -1,16 +1,6 @@
+import resourceStats from "./resource_stats_v2.json"
+
 export default [
-    { name: "Maharashtra", type: "State" },
-    { name: "Kerala", type: "State" },
-    { name: "Karnataka", type: "State" },
-    { name: "Uttar Pradesh", type: "State" },
-    { name: "Tamil Nadu", type: "State" },
-    { name: "Delhi (NCT)", type: "State" },
-    { name: "Pune", type: "District", state: "Maharashtra" },
-    { name: "Bengaluru (Bangalore) Urban", type: "District", state: "Karnataka" },
-    { name: "Mumbai City", type: "District", state: "Maharashtra" },
-    { name: "Thane", type: "District", state: "Maharashtra" },
-    { name: "Nagpur", type: "District", state: "Maharashtra" },
-    { name: "Chennai", type: "District", state: "Tamil Nadu" },
-    { name: "Nashik", type: "District", state: "Maharashtra" },
-    { name: "Ernakulam", type: "District", state: "Kerala" }
+    ...resourceStats.states?.slice(0, 6),
+    ...resourceStats.districts?.slice(0, 6)
 ];
