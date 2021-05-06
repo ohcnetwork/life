b6b92defb4ed0a0ceb44fe7b0dbd1ab9cdb99a0f
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-05-06 21:28:23 +0530

    🌋 Fix Tailwind Purge Path

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,8 @@ module.exports = {
         './pages/**/*.{js,jsx,ts,tsx}',
         './lib/**/*.{js,jsx,ts,tsx}',
         './layouts/**/*.{js,jsx,ts,tsx}',
-        './components/**/*.{js,jsx,ts,tsx}'
+        './components/**/*.{js,jsx,ts,tsx}',
+        './components/*.{js,jsx,ts,tsx}'
     ],
     darkMode: 'class',
     theme: {
