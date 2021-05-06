7661f50d4319032af1dadad8ea43f8b7f8ab0105
Shri Hari L <57325503+shrihari689@users.noreply.github.com>
2021-05-06 19:25:31 +0530

    🥗 Fix Food Filter

--- components/DetailedHome.jsx
+++ components/DetailedHome.jsx
@@ -97,6 +97,7 @@ export default function DetailedHome({ state, district, type }) {
                 .concat(this.Ambulance)
                 .concat(this.Helpline)
                 .concat(this.Vaccine)
+                .concat(this.Food)
                 .sort((record) => (isVerified(record.verification_status) ? -1 : 1));
         },
         Oxygen: getOxygen(
